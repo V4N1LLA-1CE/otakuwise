e400133f4f82dfbe65fd1e93e7c842023ce379c4
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-08-14 20:40:26 +1000

    Initial Commit

---
+++ otakuwise/otakuwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  otakuwiseApp.swift
+//  otakuwise
+//
+//  Created by Austin Sofaer on 14/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct otakuwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

433f3f526cbfaa895705fe6a6dbe154a64b824e9
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-08-15 05:28:29 +1000

    chore: add new colourset for text primary

---
+++ Otakuwise/Views/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  otakuwise
+//
+//  Created by Austin Sofaer on 15/8/2025.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
